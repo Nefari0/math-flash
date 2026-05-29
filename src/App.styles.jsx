a89aebed6592ee8751f5fd0467ce05d13f024b3e
Nefari0 <chrisdemontigny@yahoo.com>
2026-05-29 17:48:48 -0600

    Background image for offline use.

--- src/App.styles.jsx
+++ src/App.styles.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import background_image_classroom from './background_image_classroom.png'
+
 const rotation = 180
 
 export const AppContainer = styled.main`
@@ -6,8 +8,15 @@ export const AppContainer = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  align-items:center;
-  height:99.99vh;
-  width:100vw;  
-  background-image: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(https://firebasestorage.googleapis.com/v0/b/bcwb-879fa.appspot.com/o/data%2Fimages%2Fclassroom?alt=media&token=385d81a3-c878-4cfb-bdf3-5d6e5cc8e748);
-`
+  align-items: center;
+  height: 99.99vh;
+  width: 100vw;
+
+  background-image:
+    linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+    url(${background_image_classroom});
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
